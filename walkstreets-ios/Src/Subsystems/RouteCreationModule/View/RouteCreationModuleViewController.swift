badab17f9f2c4cae3553525aec0511c63d5ce4c4
Roman Ustiantcev <r.ustyantsev@excursiopedia.com>
2016-11-05 13:27:15 +0300

    pods updated

--- walkstreets-ios/Src/Subsystems/RouteCreationModule/View/RouteCreationModuleViewController.swift
+++ walkstreets-ios/Src/Subsystems/RouteCreationModule/View/RouteCreationModuleViewController.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import Mapbox
 
 class RouteCreationModuleViewController: UIViewController, RouteCreationModuleViewInput {
-
+    
+    // main view
+    @IBOutlet weak var mapView: UIView!
+    
+    // route details
+    @IBOutlet weak var routeDetailsView: UIView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var locationTypeLabel: UILabel!
+    
+    // route settings
+    @IBOutlet weak var routeSettingsView: UIView!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var routeTimerStackView: UIStackView!
+    
+    
+    
     var output: RouteCreationModuleViewOutput!
 
     // MARK: Life cycle
@@ -17,10 +35,37 @@ class RouteCreationModuleViewController: UIViewController, RouteCreationModuleVi
         super.viewDidLoad()
         RouteCreationModuleModuleConfigurator().configureModuleForViewInput(viewInput: self)
         output.viewIsReady()
+        setupInitialState()
     }
 
 
     // MARK: RouteCreationModuleViewInput
     func setupInitialState() {
+        
     }
+}
+
+extension RouteCreationModuleViewController {
+    // MARK : actions
+    
+    @IBAction func actionFindUserLocation(sender: AnyObject) {
+    
+    }
+
+    @IBAction func actionCloseRoutedetailsView(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func actionShowCloseRouteSettings(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func actionStartroute(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func actionEditRoute(sender: AnyObject) {
+        
+    }
+    
 }
