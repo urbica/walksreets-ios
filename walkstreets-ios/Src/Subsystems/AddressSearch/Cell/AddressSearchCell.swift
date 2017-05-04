469aff4cda6f498749aa6cdddc1e950da9427642
Roman Ustiantcev <liberrom.hse@gmail.com>
2017-05-04 16:11:04 +0300

    CustomizableTextField , SearchCell, search UI - added

---
+++ walkstreets-ios/Src/Subsystems/AddressSearch/Cell/AddressSearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddressSearchCell.swift
+//  walkstreets-ios
+//
+//  Created by Roman Ustiantcev on 04/05/2017.
+//  Copyright © 2017 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+class AddressSearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
