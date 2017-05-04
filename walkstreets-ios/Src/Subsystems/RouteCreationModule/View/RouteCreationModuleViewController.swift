9113f24bb7b0ad4d4268f00fd0a67da7ff74af8d
Roman Ustiantcev <liberrom.hse@gmail.com>
2017-05-04 11:44:43 +0300

    New UI added

--- walkstreets-ios/Src/Subsystems/RouteCreationModule/View/RouteCreationModuleViewController.swift
+++ walkstreets-ios/Src/Subsystems/RouteCreationModule/View/RouteCreationModuleViewController.swift
@@ -30,9 +30,16 @@ class RouteCreationModuleViewController: UIViewController, RouteCreationModuleVi
     @IBOutlet weak var lengthTimeLabel: UILabel!
     @IBOutlet weak var routeDetailsHeightConstraint: NSLayoutConstraint!
     
+    // priority views
+    @IBOutlet var priorityViews: Array<CustomizableView>!
+    @IBOutlet var priorityLabels: Array<UILabel>!
+    
     
     
     var output: RouteCreationModuleViewOutput!
+    var selectedPriorityIndex: Int?
+    var selectedTimeIndex: Int?
+    
     var routeType: String = "regular"
     var locationArray = [CLLocationCoordinate2D]()
     
@@ -55,6 +62,34 @@ class RouteCreationModuleViewController: UIViewController, RouteCreationModuleVi
         setupMap()
     }
     
+    func updatePriorityViews(index: Int) {
+        for label in priorityLabels {
+            label.textColor = UIColor.black
+        }
+        
+        for view in priorityViews {
+            view.backgroundColor = UIColor.white
+        }
+        
+        self.priorityViews[index].backgroundColor = UIColor.black
+        self.priorityLabels[index].textColor = UIColor.white
+        selectedPriorityIndex = index
+    }
+    
+    func updateSelectedTime(index: Int) {
+        for label in timeLabels {
+            label.font = UIFont(name: "VremenaGroteskBook", size: 17)
+        }
+        
+        for view in selectedTimeViews {
+            view.backgroundColor = UIColor.white
+        }
+        
+        self.selectedTimeViews[index].backgroundColor = UIColor.black
+        self.timeLabels[index].font = UIFont(name: "VremenaGroteskMedium", size: 17)
+        self.selectedTimeIndex = index
+    }
+    
 }
 
 extension RouteCreationModuleViewController {
@@ -62,6 +97,14 @@ extension RouteCreationModuleViewController {
     
     @IBAction func actionFindUserLocation(sender: AnyObject) {
     
+    }
+    
+    @IBAction func actionSelectTime(sender: UIButton) {
+        updateSelectedTime(index: sender.tag)
+    }
+    
+    @IBAction func actionSelectRoutePriority(sender: UIButton) {
+        updatePriorityViews(index: sender.tag)
     }
 
     @IBAction func actionRouteDetailsView(sender: AnyObject) {
