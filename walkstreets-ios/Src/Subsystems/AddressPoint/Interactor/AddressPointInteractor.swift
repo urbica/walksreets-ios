49113f064704a6e9170e0c0608d6910d7d71a989
Roman Ustiantcev <liberrom.hse@gmail.com>
2017-05-04 16:33:50 +0300

    PointAddress init

---
+++ walkstreets-ios/Src/Subsystems/AddressPoint/Interactor/AddressPointInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  AddressPointAddressPointInteractor.swift
+//  walkstreets-ios
+//
+//  Created by Roman Ustiantcev on 04/05/2017.
+//  Copyright © 2017 Urbica. All rights reserved.
+//
+
+class AddressPointInteractor: AddressPointInteractorInput {
+
+    weak var output: AddressPointInteractorOutput!
+
+}
---
+++ walkstreets-ios/Src/Subsystems/AddressPoint/View/AddressPointViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddressPointAddressPointViewController.swift
+//  walkstreets-ios
+//
+//  Created by Roman Ustiantcev on 04/05/2017.
+//  Copyright © 2017 Urbica. All rights reserved.
+//
+
+import UIKit
+
+class AddressPointViewController: UIViewController, AddressPointViewInput {
+
+    var output: AddressPointViewOutput!
+
+    // MARK: Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        output.viewIsReady()
+    }
+
+
+    // MARK: AddressPointViewInput
+    func setupInitialState() {
+    }
+}
